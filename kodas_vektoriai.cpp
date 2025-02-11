91e95fabb1f172599accdbce49fad58960a94330
VasareM <vasare.milkovaite@mif.stud.vu.lt>
2025-02-11 23:40:32 +0200

    sukeltos antrastes

--- kodas_vektoriai.cpp
+++ kodas_vektoriai.cpp
@@ -1,44 +1,6 @@
 #include "antrastes.h"
 
 
-void spausdinimas(vector <studentai> grupe)
-{
-    cout << std::left << setw(25) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
-    cout << string(85, '-') << endl;
-    for (const auto&m:grupe) //visi elementai is eiles is grupes; const, kad nesikopijuot7
-    {
-        cout << std::left << setw(25) << m.pavarde << setw(20) << m.vardas;
-        cout << setw(20) << std::fixed << std::setprecision(2) << m.gal_vid << setw(20) << m.gal_med << endl;
-    }
-}
-void mediana_skaiciavimas(vector <int> &pazymiai, int n, studentai &temp)
-{
-    std::sort(temp.pazymiai.begin(), temp.pazymiai.end());
-        {
-            if (n%2==0)
-            {
-                temp.mediana=((temp.pazymiai[n/2-1]+temp.pazymiai[n/2]))/2;
-            }
-            else 
-            {
-                temp.mediana=temp.pazymiai[n/2];
-            }
-        }
-        //temp.mediana=median(temp.pazymiai);
-}
-string vardo_generavimas()
-{
-    vector <string> vardu_sar={"Petras", "Jonas", "Aloyzas", "Kastytis", "Gintaras", "Tomas", "Augustas", "Dovydas", "Markas"};
-    int vardo_nr=rand()%vardu_sar.size();
-    return vardu_sar[vardo_nr];
-}
-string pavardes_generavimas()
-{
-    vector <string> pavardu_sar={"Petraitis", "Jonaitis", "Sabonis", "Zukauskas", "Stankevicius", "Maciulis", "Kudirka", "Basanavicius", "Marcinkevicius"};
-    int pavardo_nr=rand()%pavardu_sar.size();
-    return pavardu_sar[pavardo_nr];
-}
-
 int main()
 {
     srand(time(0)); //kiekvieną kartą generuojami nauji pažymiai
